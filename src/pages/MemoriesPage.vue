4d25f633bb0a3852106fd203e61d91a3ff89767f
salmaelhajraoui <salma.hajrawi@gmail.com>
2021-02-02 20:10:27 +0100

    make my first page

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,36 @@
+<template>
+    <ion-page>
+        <ion-header>
+            <ion-toolbar>
+                <ion-title>All memories</ion-title>
+            </ion-toolbar>
+        </ion-header>
+        <ion-content>
+            <ion-list>
+                <ion-item>item1</ion-item>
+                <ion-item>item2</ion-item>
+                <ion-item>item3</ion-item>
+                <ion-item>item4</ion-item>
+                <ion-item>item5</ion-item>
+                <ion-item>item6</ion-item>
+            </ion-list>
+        </ion-content>
+    </ion-page>
+
+</template>
+
+<script>
+import { IonPage, IonHeader, IonTitle, IonToolbar, IonContent, IonList, IonItem } from '@ionic/vue';
+
+export default{
+    components: {
+        IonPage,
+        IonHeader,
+        IonTitle,
+        IonToolbar,
+        IonContent,
+        IonList,
+        IonItem,
+    }
+}
+</script>
